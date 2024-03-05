d697b67b9820c4f6c57a85f27b89b0592b8cb2d1
Rehan Sheikh <rs2126@cam.ac.uk>
2024-03-05 16:57:11 +0000

    Added CORS to flask app

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask
 from .config import Config
 from .routes.compute_embedding import compute_embedding_bp
 from .routes.gpt_4V import gpt_4v_bp
+from flask_cors import CORS
 
 
 def create_app(config_name=None):
     app = Flask(__name__)
+    CORS(app)
 
     if config_name == 'testing':
         app.config.from_object('app.config.TestingConfig')
